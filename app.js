5e8d16001fff13d171cab00b99da6dcfe76597dc
Chrizzz42 <christoffer@allright.dk>
2017-09-09 20:43:12 +0200

    Several fragile implementations.

--- app.js
+++ app.js
@@ -1,52 +1,181 @@
 const express = require('express');
 const ip = require('ip');
 const crypto = require('crypto');
+const winston = require("winston");
+const async = require("async");
+const request = require("request");
 
 const app = express();
+
+winston.level = "debug";
+winston.debug("Logging in debug mode");
+
+class Triple {
+    constructor(ip, port, id) {
+        this.ip = ip;
+        this.port = port;
+        this.id = id;
+    }
+
+    toString() {
+        return "<" + this.ip + "," + this.port + "," + this.id + ">"
+    }
+}
+
+class Bucket {
+    constructor(triple, k) {
+        this.triples = [triple];
+        this.k = k;
+    }
+
+    contains(triple) {
+        return this.triples.map(JSON.stringify).includes(JSON.stringify(triple))
+    }
+
+    put(triple) {
+        if(this.triples.length < k) {
+            this.triples.push(triple);
+        }
+        else if(!this.contains(triple)) {
+            leastRecentTriple = this.triples[this.triples.length-1];
+            let options = {
+                host: triple.address,
+                port: triple.port,
+                path: "/api/kademlia/ping",
+                method: "POST"
+            };
+            request(options, (err,res,body) => {
+                winston.debug("PING of" + triple.id + " returned response " + res.statusCode);
+                if(err || res.statusCode != 200) {
+                    this.triples[this.triples.length] = triple;
+                }
+            });
+        }
+        else {
+            let index = this.triples.map(JSON.stringify).indexOf(JSON.stringify(triple));
+            this.triples = this.triples.splice();
+            this.triples[this.triples.length] = triple;
+        }
+    }
+
+}
+
 app.set("view engine", "pug");
 
-const port = 3000;
-const alpha = 3;
-const B = 8;
-const k = 10;
+const port = process.argv[2] ? parseInt(process.argv[2]) : 3000;
+const alpha = process.argv[3] ? parseInt(process.argv[3]) : 3;
+const B = process.argv[4] ? parseInt(process.argv[4]) : 8;
+const k = process.argv[5] ? parseInt(process.argv[5]) : 10;
 var id = undefined;
-
+generateId();
 var buckets = [];
+var testTriple = new Triple("192.168.0.114","3000","113");
+putTripleInBucket(testTriple);
 
 /**
  * Joins the kademlia network.
  */
 app.post('/api/kademlia/join', (req, res) => {
-
-    // Generate ID
-    generateId();
-
     // Put known in bucket
-    
+    var joinTriple 
+        = new Triple(req.header("node_address"), 
+                    req.header("node_port"), 
+                    req.header("node_id"));
+    putTripleInBucket(joinTriple);
+    winston.info("Attempting to join " + joinTriple.toString());
     // iterateFindNode on  this  })
+    nodeLookup(id);
     res.send("ok");
 })
 
+/**
+ * Overview page
+ */
 app.get('/api/kademlia', (req, res) =>{
-    if(id != undefined) res.render("index", {nodeid: id, bucketlist: buckets});
-    else res.render("notInNetwork", {address: ip.address() + ":" + port});
+    if(buckets.length != 0) res.render("index", {nodeid: id, bucketlist: buckets});
+    else res.render("notInNetwork", {address: ip.address() + ":" + port, nodeID: id});
+    winston.info("Node overview accessed by " + req.hostname);
 })
 
+/**
+ * FIND_NODE
+ */
 app.get('/api/kademlia/nodes/:id', (req, res) => {
-    var id = req.params["id"];  
-    res.send(id);
+    var reqID = parseInt(req.params["id"]);
+    winston.debug("Requested ID: " + reqID);
+    var closest = getNClosest(reqID, k);
+    const randID = req.get("ID");
+    res.contentType("application/json");
+    res.setHeader("ID", randID);
+    res.statusCode = 200;
+    res.send(JSON.stringify(closest));
+    winston.info("FIND_NODE(" + reqID + ") returned " + closest)
 })
 
 app.post('/api/kademlia/ping', (req,res) => {
     const randID = req.get("ID");
-    res.writeHead(200 , {"ID": randID});
+    res.writeHead(200 , {"ID": randID}); // Remember to return random ID
+    let triple = new Triple(req.header("node_address"),req.header("node_port"),req.header("node_id"));
+    putTripleInBucket(triple);
     res.send();
+    winston.info("Ping recieved from " + req.hostname);
 })
 
 app.listen(port, () => {
-    console.log('Example app listening on port 3000!')
-})  
+    console.log('Kademlia node listening on port ' + port + "!")
+})
 
+/**
+ * Technically iterativeNodeLookup
+ * @param {*} id 
+ */
+
+function nodeLookup(reqID) {
+    let closestNode = undefined;
+    let closest = getNClosest(reqID, alpha);
+    // Perform alpha async FIND_NODE calls
+    let makeReq = function(triple){
+        let options = {
+            host: triple.address,
+            path: "/api/kademlia/nodes/" + triple.id,
+            port: triple.port,
+            ID: Math.random()*Math.pow(2,B), //TODO: This is a placeholder. Replace this with actual safe randomizer,
+            node_id: id,
+            node_port: port,
+            node_address: ip.address()
+        };
+        request.get(options, (err, res, body) => {
+            if(!err && res.statusCode == 200) {
+                let results = JSON.parse(body);
+                return results;
+            }
+        })
+    }
+    let allResults = []; //Put actual results here (remember to exclude duplicates)
+
+    // Aggregate k closest results (sort results by distance and take the first k)
+    let kBestResults = allResults.sort((x, y) => (distance(reqID, x.id) - distance(reqID, id.y))).splice(0,k);
+    // Perform async FIND_NODE on alpha closest of the k
+    // Continue until nothing new is established
+    return closestNode;
+}
+
+function getNClosest(reqID, n) {
+    let dist = distance(id, reqID);
+    let bucketNumber = dist == 0 ? 0 : Math.floor(Math.log2(dist));
+    let closest = [];
+    for(let i = bucketNumber; i <= B; i++) {
+        if(closest.length < n) {
+            if(buckets[i] instanceof Bucket) buckets[i].triples.forEach(triple => {
+                if(closest.length < n) {
+                    closest.push(triple);
+                }
+            })
+        }
+        else break;
+    }
+    return closest;
+}
 
 /**
  * https://stackoverflow.com/questions/45053624/convert-hex-to-binary-in-javascript
@@ -64,36 +193,31 @@ function bin2Dec(bin) {
     return parseInt(bin,2).toString(10)
 }
 
-
+/**
+ * Generates this node's ID and stores it in the global "id"
+ */
 function generateId() {
     var address = ip.address();
-    const hash = crypto.createHash("sha256", address + port).digest("hex");
+    const hash = crypto.createHash("sha256").update(address + port).digest("hex");
+    winston.debug("Hash for " + address + ":" + port + " = " + hash );
     const bin = hex2bin(hash);
     const sub = bin.substring(0, B-1);
     const binId = parseInt(sub);
     id = bin2Dec(binId);
+    winston.info("Node ID: " + id);
 }
 
 function putTripleInBucket(triple) {
-    const distance = distance(id, triple.id);
-    const i = Math.floor(getBaseLog(2, distance));
+    const dist = distance(id, triple.id);
+    const i = dist == 0 ? 0 : Math.floor(Math.log2(dist));
 
-    if (buckets[i]) {
+    if (buckets[i] instanceof Bucket) {
         buckets[i].put(triple);
     } else {
-        buckets[i] = new Bucket(triple);
+        buckets[i] = new Bucket(triple, k);
     }
 }
 
 function distance(a, b) {
-    return a ^ b;
-}
-
-/**
- * The following function returns the logarithm of y with base x (ie. logxy).
- * @param {*} x 
- * @param {*} y 
- */
-function getBaseLog(x, y) {
-    return Math.log(y) / Math.log(x);
+    return parseInt(a) ^ parseInt(b);
 }
