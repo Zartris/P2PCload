6d966f51b0ef59ddb636f3beb0b335e7c5bd88ac
JonasSL <jonaslarsen1994@gmail.com>
2017-09-13 13:02:09 +0200

    Added node_modules/* to .gitignore. Added documentation (apidoc).

---
+++ doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "Kademlia API",  "version": "1.0.0",  "description": "Kademlia implementation",  "title": "Kademlia API",  "url": "",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-09-13T10:56:48.550Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
