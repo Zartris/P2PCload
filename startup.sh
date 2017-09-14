afde4b26a0aa8eb0efc22852e38ca1614b804199
Unknown <201303833@post.au.dk>
2017-09-14 15:59:03 +0200

    Script launches nodes in same terminal instance
    Added "&" to the end of the execution of app.js.
    & launches a program in the background.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 # Basic range in for loop
 for value in {1..10}
 do
-    x-terminal-emulator -e node app.js $((3003 + $value)) $1 3003 113
+    node app.js $((3003 + $value)) $1 3003 113 &
     sleep 1
 done
