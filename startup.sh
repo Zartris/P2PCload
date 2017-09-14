9193b34a14290583a747fc389bc52a77859432e7
Unknown <201303833@post.au.dk>
2017-09-14 14:17:51 +0200

    Script fix
    Parameterized it.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 # Basic range in for loop
 for value in {1..10}
 do
-    x-terminal-emulator -e node app.js $((3003 + $value)) 10.192.112.92 3003 113
+    x-terminal-emulator -e node app.js $((3003 + $value)) $1 3003 113
     sleep 1
 done
